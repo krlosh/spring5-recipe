a0e25396120485b27c22c688ef7766f4554f9348
krlosh <krlosh@gmail.com>
2018-04-20 21:04:37 +0200

    Append Unit of measure and relation with ingredient

--- src/main/java/guru/springframework/recipe/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipe/domain/Ingredient.java
@@ -14,7 +14,9 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
-    //TODO: UnitOfMeasure
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -46,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
