1e4d1e2e47888466842cd1606482c56386bb2cfc
krlosh <krlosh@gmail.com>
2018-04-22 13:00:46 +0200

    Use data.sql to populate initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category(description) values ('American');
+INSERT INTO category(description) values ('Italian');
+INSERT INTO category(description) values ('Mexican');
+INSERT INTO category(description) values ('Fast food');
+
+INSERT INTO unit_of_measure(description) values('Teaspon');
+INSERT INTO unit_of_measure(description) values('Tablespon');
+INSERT INTO unit_of_measure(description) values('Cup');
+INSERT INTO unit_of_measure(description) values('Pinch');
+INSERT INTO unit_of_measure(description) values('Ounce');
