b33df66b3e1c37b0cc14d99a90226d18680b53a0
krlosh <krlosh@gmail.com>
2018-04-22 12:34:54 +0200

    Append Category and recipe relations

--- src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -1,5 +1,7 @@
 package guru.springframework.recipe.domain;
 
+import org.hibernate.annotations.ManyToAny;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -27,6 +29,12 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name="recipe_category",
+        joinColumns = @JoinColumn(name="recipe_id"),
+        inverseJoinColumns = @JoinColumn(name="category_id") )
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
