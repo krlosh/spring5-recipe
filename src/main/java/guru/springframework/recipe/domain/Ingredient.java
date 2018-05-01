cc9cabbbc41c09c777849537aa891a94778b8d85
krlosh <krlosh@gmail.com>
2018-05-01 12:42:12 +0200

    Refactor some recipe setters to define both sides in bidirectional relation

--- src/main/java/guru/springframework/recipe/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipe/domain/Ingredient.java
@@ -8,7 +8,13 @@ public class Ingredient {
 
     public Ingredient() {}
 
-    public Ingredient(String description, BigDecimal amount,  UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
--- src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -116,6 +116,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -124,6 +125,12 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Difficulty getDifficulty() {
