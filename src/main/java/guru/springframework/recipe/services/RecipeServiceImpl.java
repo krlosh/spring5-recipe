e65215b0036b788fe51d2cf2cf9ca714349dd8b2
krlosh <krlosh@gmail.com>
2018-05-01 13:26:44 +0200

    Append lombok dependencie and some refactors to use it

--- src/main/java/guru/springframework/recipe/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipe/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package guru.springframework.recipe.services;
 
 import guru.springframework.recipe.domain.Recipe;
 import guru.springframework.recipe.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
+@Slf4j
 @Service
 public class RecipeServiceImpl implements  RecipeService {
 
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements  RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         this.recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
