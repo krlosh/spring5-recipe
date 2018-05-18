6ee03eb76460688df476215749ac70cece690114
krlosh <krlosh@gmail.com>
2018-05-18 18:32:44 +0200

    Created Command objects

---
+++ src/main/java/guru/springframework/recipe/commands/IngredientCommand.java
@@ -0,0 +1,16 @@
+package guru.springframework.recipe.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@NoArgsConstructor
+public class IngredientCommand {
+    private Long id;
+    private BigDecimal amount;
+    private String description;
+    private UnitOfMeasureCommand uom;
+}
